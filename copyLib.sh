dfe854e3bd8b49884d905c475a05b3f7f2cdb010
Walter Paulo <walter0paulo@htmai.com>
2021-06-03 21:22:32 -0300

    Primeiro commit

---
+++ copyLib.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+# findLib.sh - Nosso Program em Shell - Procura e copia lib para o diretório
+#
+# Homepage: http://utah.com.br
+# Autor: Walter Paulo <walter0paulo@hotmail.com>
+# Mantenedor: Walter Paulo
+#
+#--------------------------------------------------------
+#
+# Este programa irá procurar as bibliotecas essencial para o comando. O usuário
+# digita o comando e diretório. 
+#
+#
+# $ ./copyLib.sh ls /home/walter/chroot
+# 
+#  
+#
+# Hitório de Versões
+#
+# Versão: 1.0
+#
+# COPYRIGHT: Este programa é GPL
+
+
+CAMANDO=$1
+LIB=$(which $CAMANDO|xargs -I %s ldd %s|grep -o "/lib.*\.[0-9]")
+FILEDIR=$2
+ERROR=""
+
+
+NEWDIR(){
+[ ! -d $FILEDIR ] &&
+	mkdir -p $FILEDIR
+}
+
+VALIDAR_COMANDO(){
+	[[ -n "$(which $CAMANDO)" ]] || ERROR="\e[31m+ \e[m\e/Camando não existem;\n"
+	[[ -n "$FILEDIR" ]] || ERROR+="\e[31m+ \e[m\e/Diretório não informado;"
+}
+
+VALIDAR_COMANDO
+
+if [ "$ERROR" ]; then
+	echo
+	echo -e $ERROR
+	echo
+else
+	NEWDIR
+	cp -fva $LIB $FILEDIR
+fi
+exit 0
+
+
